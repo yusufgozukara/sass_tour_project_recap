6f3f2b2f2e2fe17070ec50b39573b506026cc8e5
yusufgozukara <yusufgozukara11@gmail.com>
2023-05-18 22:23:36 +0300

    header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 };
