ed3ac4471702ea0c3829a5b56f692c637baf05cd
yusufgozukara <yusufgozukara11@gmail.com>
2023-05-18 22:13:35 +0300

    navbar finished

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <h1>Welcome to my world</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi quod
-        doloremque nesciunt quos doloribus molestias, possimus quas, vitae porro
-        inventore debitis ipsam maiores, quae eius facere. Eveniet, ducimus
-        libero rem nam iusto officia corporis quasi eius rerum explicabo culpa
-        optio.
-      </p>
     </div>
   );
 };
